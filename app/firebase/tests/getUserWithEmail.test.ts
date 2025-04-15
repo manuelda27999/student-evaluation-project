0309192149490db6b36199a6c21974d047825536
manuelda27999 <manuelda27999@gmail.com>
2025-04-15 18:35:42 +0200

    get user with email and try to delete user

---
+++ app/firebase/tests/getUserWithEmail.test.ts
@@ -0,0 +1,26 @@
+//Test to login a user
+import getUserWithEmail from "../logic/getUserFromStore";
+import registerUser from "../logic/registerUser";
+import { getAuth, signOut } from "firebase/auth";
+
+describe("getUser", () => {
+  afterAll(async () => {
+    await signOut(getAuth());
+  });
+
+  test("get user with the email", async () => {
+    const name = "ExampleName";
+    const email = `test${Date.now()}@example.com`;
+    const password = Math.floor(Math.random() * 1000000).toString();
+    const role = "student";
+
+    await registerUser(name, email, password, role);
+
+    const result = await getUserWithEmail(email);
+
+    expect(result).toHaveProperty("name", name);
+    expect(result).toHaveProperty("email", email);
+    expect(result).toHaveProperty("password", password);
+    expect(result).toHaveProperty("role", role);
+  });
+});
