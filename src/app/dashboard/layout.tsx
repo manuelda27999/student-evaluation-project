d864358ec5b45383cc4c3705abee04d248600ec5
manuelda27999 <manuelda27999@gmail.com>
2025-05-15 18:03:42 +0200

    update layout to redirect to the correct path according to the screen size

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import DesktopMenu from "@/components/desktop_modules/DesktopMenu";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRedirect = () => {
+      if (window.innerWidth < 768) {
+        router.replace("/mobile/menu");
+      }
+    };
+
+    handleRedirect();
+
+    window.addEventListener("resize", handleRedirect);
+    return () => window.removeEventListener("resize", handleRedirect);
+  }, [router]);
+
   return (
     <main className="h-screen w-screen pt-16 overflow-hidden">
       <section className="h-full flex flex-row">
